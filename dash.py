3a8e00a26bd5d676777b570add237a23c5b4cac5
Hostílio Neto <netocajeh@gmail.com>
2025-03-22 18:43:23 -0300

    adicionado codigo de leitura da planilha

--- dash.py
+++ dash.py
@@ -2,4 +2,10 @@ import streamlit as st
 import pandas as pd 
 import plotly.express as px 
 
-df = pd.read_csv('/produtos_servicos.csv')
+st.set_page_config(layout='wide')
+
+df = pd.read_csv('produtos_servicos.csv', sep=";", decimal=",")
+
+df
+
+
