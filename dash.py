09d204a9f9842817b73e0a60c262879c451e7d66
Luiz Luna <luizluna.dev@gmail.com>
2025-03-22 23:51:16 -0300

    Adicionar gráfico por tipo de venda

--- dash.py
+++ dash.py
@@ -8,3 +8,6 @@ df = pd.read_csv('produtos_servicos.csv', sep=";", decimal=",", parse_dates=True
 
 col1, col2 = st.columns(2)
 col3, col4 = st.columns(2)
+
+fig_tipo = px.pie(df, names="Tipo", title="Distribuição por tipo de venda")
+col1.plotly_chart(fig_tipo)
