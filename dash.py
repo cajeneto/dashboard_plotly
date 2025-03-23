bd1f28c8f1bb3ac175391286f21c24b43dd0aaa7
Luiz Luna <luizluna.dev@gmail.com>
2025-03-22 23:49:20 -0300

    Adicionar 4 caixas para preenchimento dos gráficos

--- dash.py
+++ dash.py
@@ -4,8 +4,7 @@ import plotly.express as px
 
 st.set_page_config(layout='wide')
 
-df = pd.read_csv('produtos_servicos.csv', sep=";", decimal=",")
+df = pd.read_csv('produtos_servicos.csv', sep=";", decimal=",", parse_dates=True, dayfirst=True)
 
-df
-
-
+col1, col2 = st.columns(2)
+col3, col4 = st.columns(2)
